746b57ff5ba3fabe5209996b3008ad67a4a8024a
heeropunjabi <heeropunjabi@gmail.com>
2018-08-13 01:00:18 +0530

    jest - commit

---
+++ users.test.js
@@ -0,0 +1,16 @@
+jest.mock("request");
+
+import * as user from "./users";
+
+// The assertion for a promise must be returned.
+test("should ", () => {
+  expect.assertions(1);
+  return expect(user.getUserName(4)).rejects.toEqual({
+    error: "User with 3 not found."
+  });
+});
+it("works with async/await", async () => {
+  expect.assertions(1);
+  const data = await user.getUserName(4);
+  expect(data).toEqual("Mark");
+});
